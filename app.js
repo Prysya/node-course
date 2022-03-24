4f8e2ee26dfd650d6348e946c0f3a57e70c17dd1
Stas <bloodofabyss@yandex.ru>
2022-03-24 23:26:37 +0300

    feat: подключил ендпоинт ошибок

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-const express = require('express');
-const router = require('./routes');
+const express = require("express");
 
-require('dotenv').config();
+const router = require("./routes");
+const { errorHandler } = require("./middlewares");
+
+require("dotenv").config();
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 app.use(router);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Слушаю порт ${PORT}`);
