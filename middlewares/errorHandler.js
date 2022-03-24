f499faa3314fee023cfcfb0feec1aa6867899a96
Stas <bloodofabyss@yandex.ru>
2022-03-24 23:25:14 +0300

    feat: обновление после prettier

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -4,6 +4,6 @@ module.exports = (err, req, res, next) => {
   res.status(statusCode).send({
     message: statusCode === 500 ? "На сервере произошла ошибка" : message,
   });
-  
-  next()
+
+  next();
 };
