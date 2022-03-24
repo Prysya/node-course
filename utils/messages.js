fc1dbebb6dfdc6fd4c1340b61f01139d62b6f397
Stas <bloodofabyss@yandex.ru>
2022-03-24 23:23:16 +0300

    feat: добавил сообщение об ошибке если книга не создана

--- utils/messages.js
+++ utils/messages.js
@@ -1,6 +1,7 @@
 module.exports = {
   errors: {
     notFound: "Запрашиваемый ресурс не найден",
+    bookNotCreate: 'Ошибка при создании книги'
   },
   success: {
     dataUpdateSuccess: 'Данные успешно обновлены',
