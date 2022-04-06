5c59b70050ac86c8ee74cae82f5f4343d66d3968
Stas <bloodofabyss@yandex.ru>
2022-04-06 21:42:57 +0300

    feat: перенес весь проект в src, написал функционал для работы со слоем каунтера

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const router = require("./routes");
 const { errorHandler } = require("./middlewares");
@@ -10,11 +11,14 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: false
-}))
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(router);
 
--- src/models/book.js
+++ src/models/book.js
@@ -4,7 +4,6 @@ const {
   randMovie,
   randLine,
   randFullName,
-  randDirectoryPath,
   randBoolean,
 } = require("@ngneat/falso");
 
