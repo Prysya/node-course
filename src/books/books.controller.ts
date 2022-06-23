346d635129be2ff714c6c8b5053d541d0d039b19
Stas <bloodofabyss@yandex.ru>
2022-06-23 12:58:25 +0300

    feat: подключил mongodb и сервисы для работы с БД

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
 import { BooksService } from './books.service';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
 
 @Controller('books')
 export class BooksController {
@@ -7,6 +9,26 @@ export class BooksController {
 
   @Get()
   findAll() {
-    return this.booksService.getAll();
+    return this.booksService.findAll();
+  }
+
+  @Post()
+  create(@Body() createBookDto: CreateBookDto) {
+    return this.booksService.create(createBookDto);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.booksService.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateBookDto: UpdateBookDto) {
+    return this.booksService.update(id, updateBookDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.booksService.delete(id);
   }
 }
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,11 +1,41 @@
 import { Injectable } from '@nestjs/common';
-import { Book } from './book.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
+import { Model } from 'mongoose';
+import { Book, BookDocument } from './schemas/book.schema';
 
 @Injectable()
 export class BooksService {
-  private readonly books: Book[] = [];
+  constructor(
+    @InjectModel(Book.name) private readonly bookModel: Model<BookDocument>,
+  ) {}
 
-  getAll() {
-    return this.books;
+  async create(createBookDto: CreateBookDto): Promise<Book> {
+    const createdBook = await this.bookModel.create(createBookDto);
+
+    return createdBook;
+  }
+
+  async findAll(): Promise<Book[]> {
+    return this.bookModel.find().exec();
+  }
+
+  async findOne(id: string): Promise<Book> {
+    return this.bookModel.findOne({ _id: id }).exec();
+  }
+
+  async update(id: string, updateBookDto: UpdateBookDto): Promise<Book> {
+    return this.bookModel.findOneAndUpdate({ _id: id }, updateBookDto, {
+      lean: true,
+      returnDocument: 'after',
+    });
+  }
+
+  async delete(id: string) {
+    const deletedBook = await this.bookModel
+      .findByIdAndRemove({ _id: id })
+      .exec();
+    return deletedBook;
   }
 }
