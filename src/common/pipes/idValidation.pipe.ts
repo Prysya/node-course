7c4f7cb76637938180e7c80af66ca4a63966f8d6
Stas <bloodofabyss@yandex.ru>
2022-07-02 15:42:34 +0300

    feat: написал id validation pipe

---
+++ src/common/pipes/idValidation.pipe.ts
@@ -0,0 +1,13 @@
+import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
+import mongoose from 'mongoose';
+
+@Injectable()
+export class IdValidationPipe implements PipeTransform<string> {
+  public transform(value: string) {
+    if (mongoose.isValidObjectId(value)) {
+      return value;
+    }
+
+    throw new BadRequestException('id не валиден')
+  }
+}
