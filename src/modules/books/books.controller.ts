ce0ebd5c959fd531877a78477c0930156f9e335b
Stas <bloodofabyss@yandex.ru>
2022-07-02 15:44:57 +0300

    feat: подключил Validation пайпы в контроллер книг

--- src/modules/books/books.controller.ts
+++ src/modules/books/books.controller.ts
@@ -1,7 +1,10 @@
-import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Put, UsePipes } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { IdValidationPipe } from '../../common/pipes/idValidation.pipe';
+import { JoiValidationPipe } from '../../common/pipes/joiValidation.pipe';
+import { createBookSchema } from '../../modules/books/joi/createBook.schema';
 
 @Controller('books')
 export class BooksController {
@@ -13,6 +16,7 @@ export class BooksController {
   }
 
   @Post()
+  @UsePipes(new JoiValidationPipe(createBookSchema))
   create(@Body() createBookDto: CreateBookDto) {
     return this.booksService.create(createBookDto);
   }
@@ -23,7 +27,7 @@ export class BooksController {
   }
 
   @Put(':id')
-  update(@Param('id') id: string, @Body() updateBookDto: UpdateBookDto) {
+  update(@Param('id', new IdValidationPipe()) id: string, @Body() updateBookDto: UpdateBookDto) {
     return this.booksService.update(id, updateBookDto);
   }
 
