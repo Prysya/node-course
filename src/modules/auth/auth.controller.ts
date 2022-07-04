b3c2dfdafb4c20bf3e76df6b2c13ca6ee9f59668
Stas <bloodofabyss@yandex.ru>
2022-07-04 18:34:58 +0300

    feat: реализовал auth модуль

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,26 @@
+import { Body, Controller, Post, Request,  } from '@nestjs/common';
+import { CreateUserDto } from '../users/dto/create-user.dto';
+import { UsersService } from '../users/users.service';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(
+    private readonly userService: UsersService,
+    private readonly authService: AuthService
+  ) {
+  }
+  @Post('signin')
+  async signin(@Request() req: any, @Body() body: Body & {email: string, password: string}) {
+    const user = await this.userService.findUser(body.email, body.password)
+
+    return this.authService.sendToken(user);
+  }
+
+  @Post('signup')
+  async signup(@Body() body: CreateUserDto) {
+    const user = await this.userService.createUser(body);
+
+    return this.authService.sendToken(user)
+  }
+}
---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
+
+import { UsersModule } from '../../modules/users/users.module';
+import { AuthController } from './auth.controller';
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
+  controllers: [AuthController],
+})
+export class AuthModule {}
